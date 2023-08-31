921ba7c2f44f7407678bb39cceb2a921dc870929
Maryline-Bellanger <maryline.bellanger45@gmail.com>
2023-08-31 13:05:00 +0200

    create the home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-[url('/images/background_starwars.jpg')] bg-cover">
       <body>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import Image from 'next/image'
+import Image from "next/image";
+import logo from "../../public/images/starwars_logo.png"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>Home page</div>
+    <div className="flex flex-col items-center py-5">
+        <Image src={logo} alt="logo starwars" width={200} className="py-5" />
+        <h1 className="text-yellow-400 text-3xl p-5">A long time ago in a galaxy far, far away...</h1>
+        <div className="py-5">
+            <button className="btn bg-neutral-800 border-neutral-500 opacity-80 hover:bg-neutral-900">
+                <Link href={"/"} className="text-xl text-yellow-200">
+                    Enter
+                </Link>
+            </button>
+        </div>
+    </div>
   )
 }
