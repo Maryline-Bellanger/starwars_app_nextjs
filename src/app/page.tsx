4c4212056c2ae141f405a393ac7b74968c2d6cff
Maryline-Bellanger <maryline.bellanger45@gmail.com>
2023-09-02 14:08:11 +0200

    modify style from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center py-5">
-        <Image src={logo} alt="logo starwars" width={200} className="py-5" />
-        <h1 className="text-yellow-400 text-3xl p-5">A long time ago in a galaxy far, far away...</h1>
-        <div className="py-5">
+    <div className="flex flex-col items-center h-screen justify-center">
+        <Image src={logo} alt="logo starwars" width={200} className="pb-5" />
+        <h1 className="text-yellow-400 text-3xl p-5 text-center">A long time ago in a galaxy far, far away...</h1>
+        <div className="pt-5">
             <button className="btn bg-neutral-800 border-neutral-500 opacity-80 hover:bg-neutral-900">
                 <Link href={"/characters"} className="text-xl text-yellow-200">
                     Enter
