def2ab01aa39fcf876af523eee508b994d38a3f4
Maryline-Bellanger <maryline.bellanger45@gmail.com>
2023-10-27 18:34:59 +0200

    add error image

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,4 +1,6 @@
+"use client"
 import Image from "next/image"
+import { useState } from "react"
 
 type cardData = {
     id: string,
@@ -7,13 +9,16 @@ type cardData = {
 }
 
 export default function Card({name, image, id}: cardData) {
+    const [imgSrc, setImgSrc] = useState(image);
+
   return (
     <div key={id} className="card w-40 bg-base-100 shadow-xl m-3">
             <Image
-                src={image}
-                alt="Image not found"
+                src={imgSrc}
+                alt=""
                 width={200}
                 height={200}
+                onError={() => setImgSrc('/images/image_not_found.png')}
                 className="rounded-t-xl bg-black text-center" />
         <div className="py-3 text-black font-semibold items-center text-center text-xs">
             {name}
